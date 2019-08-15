296eecdefbb0b11abc6403b5af8bfab19ef8b3aa
Edward S. Rice <erice11@unl.edu>
2019-08-14 18:15:01 -0700

    Finished compose module

--- compose.py
+++ compose.py
@@ -4,7 +4,7 @@ Functions for composing two AGPs together.
 
 import collections
 from functools import partial
-from itertools import filterfalse
+from itertools import chain
 
 class BrokenComponentError(Exception):
     def __init__(self, outer_row, inner_row):
@@ -19,6 +19,14 @@ class BrokenComponentError(Exception):
                                   self.inner_row.object,
                                   self.inner_row.object_beg,
                                   self.inner_row.object_end)
+
+
+def ComponentNotDefinedError(Exception):
+    def __init__(self, component_name):
+        self.component_name = component_name
+
+    def __str__(self):
+        return 'Component "{}" not defined.'.format(self.component_name)
 
 
 def contains(outer_row, inner_row):
@@ -41,10 +49,11 @@ def contains(outer_row, inner_row):
         return False
 
 
-def run(outer_agp, inner_agp, outfile):
+def run(outer_agp, inner_agp, outfile, print_unused=False):
     """runs the compose module of agptools"""
     # read the inner agp into a dictionary so that we can look up the
-    # definition of an outer component later
+    # definition of an outer component later. inner_dict maps object
+    # names to a list of all AgpRow instances with that object name
     inner_dict = collections.defaultdict(list)
     for inner_row in filter(lambda r: not isinstance(r, str), inner_agp):
         inner_dict[inner_row.object].append(row)
@@ -73,22 +82,33 @@ def run(outer_agp, inner_agp, outfile):
         else:
             # check to make sure this component is actually defined
             if outer_row.component_id not in inner_dict:
-                raise Exception # TODO specify!
+                raise ComponentNotDefinedError(outer_row.component_id)
 
-            # make a list of all rows in this component, within range
+            # make a list of all rows in this component and make sure
+            # the object is using the whole component
             inner_rows = inner_dict[outer_row.component_id]
-            component_rows = filter(partial(contains, outer_row), inner_rows)
-
-            # remove anything we're using now from inner_dict, as we
-            # will later be printing all components that did not get
-            # used
-            inner_dict[outer_row.component_id][:] = filterfalse(
-                    partial(contains, outer_row), inner_rows)
+            del inner_dict[outer_row.component_id]
+            if (outer_row.component_beg != 1
+                    or outer_row.component_end != inner_rows[-1].object_end):
+                raise BrokenComponentError(outer_row, inner_rows[-1])
 
             # reverse complement everything if necessary
             if outer_row.orientation == '-':
-                component_rows = flip.reverse_rows(component_rows)
+                inner_rows = flip.reverse_rows(inner_rows)
 
-            # TODO fix offsets
+            # fix offsets and print rows
+            for inner_row in inner_rows:
+                inner_row.object = outer_row.object
+                inner_row.object_beg += outer_row.object_beg - 1
+                inner_row.object_end += outer_row.object_beg - 1
+                inner_row.part_number = component_counter
+                component_counter += 1
+                print(inner_row, file=outfile)
+
+    # at the end, print all the objects in the inner AGP that were not
+    # assembled into anything in the outer AGP
+    if print_unused:
+        for row in chain.from_iterable(inner_dict.values()):
+            print(row, file=outfile)
 
 

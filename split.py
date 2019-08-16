9df5e07e15fd4378b8352c177b3d5eea17c109a0
Edward S. Rice <erice11@unl.edu>
2019-08-16 13:14:03 -0700

    Made new function for splitting contigs

--- split.py
+++ split.py
@@ -2,6 +2,8 @@
 Functions for splitting a scaffold into subscaffolds at gaps.
 """
 
+from copy import deepcopy
+
 class BreakpointError(Exception):
     def __init__(self, scaffold_name, bad_breakpoints):
         self.scaffold_name = scaffold_name
@@ -65,6 +67,44 @@ def unoffset_rows(new_scaffold_name, rows):
         row.part_number -= part_number_offset
         out_rows.append(row)
     return out_rows
+
+
+def split_contig(contig_row, breakpoints):
+    """
+    Splits a single row containing a contig into multiple rows,
+    each containing a piece of the contig.
+
+    >>> import agp
+    >>> r = agp.AgpRow('\\t'.join(map(str, ['scaf', 501, 1000, 5, 'W',
+    ...                                    'ctg', 1, 500, '+'])))
+    >>> list(map(lambda x: str(x).split('\\t'),
+    ...          split_contig(r, [750, 867])))
+    [['scaf', '501', '750', '5', 'W', 'ctg', '1', '250', '+'],
+     ['scaf', '751', '867', '6', 'W', 'ctg', '251', '367', '+'],
+     ['scaf', '868', '1000', '7', 'W', 'ctg', '368', '500', '+']]
+
+    Args:
+        contig_row (AgpRow): a single row to be split
+        breakpoints (list(int)): positions where contig should be split,
+            in object coordinates, *not* component coordinates. The left
+            part of the split includes the breakpoint: e.g., splitting a
+            contig of length 100 at 43 will make two new contigs: one
+            from 1-43 and the other from 44-100.
+    """
+    rows = [contig_row]
+    for breakpoint in sorted(breakpoints):
+        left_part = deepcopy(rows.pop())
+        right_part = deepcopy(left_part)
+
+        left_part.object_end = breakpoint
+        right_part.object_beg = breakpoint + 1
+        right_part.part_number += 1
+        left_part.component_end = (left_part.component_beg
+                                   + (breakpoint - left_part.object_beg))
+        right_part.component_beg = left_part.component_end + 1
+
+        rows += [left_part, right_part]
+    return rows
 
 
 def split_scaffold(scaffold_rows, breakpoints):
@@ -146,3 +186,6 @@ def run(breakpoints, outfile, agp):
     for r in rows_this_scaffold: print(r, file=outfile)
 
 
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod(optionflags=doctest.NORMALIZE_WHITESPACE)

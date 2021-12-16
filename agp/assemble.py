9b0c3542a7287f0b23be98ce25fe2ebd2f25d7fe
Edward S. Rice <edward.rice@missouri.edu>
2021-12-16 19:54:07 +0100

    fix doctest

--- agp/assemble.py
+++ agp/assemble.py
@@ -52,6 +52,7 @@ def print_fasta(name: str, sequence: str, outfile: IO, wrap: int = 60):
 
     Returns: the sequence in fasta format
 
+    >>> import sys
     >>> print_fasta('chr1', 'ATCGACTGATCGACTGACTGACTACTG', outfile=sys.stdout, wrap=10)
     >chr1
     ATCGACTGAT

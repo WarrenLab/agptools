d0a7bda7b20342e7d49e80d86e731919e554b747
Edward S. Rice <edward.rice@missouri.edu>
2022-03-17 15:48:45 +0100

    Fixed bug in transform module
    Transforming bed ranges that within broken contigs led to negative
    coordinates and other errors. Should be fine now.

--- agp/transform.py
+++ agp/transform.py
@@ -2,8 +2,11 @@
 Functions for transforming genomic coordinates from contig-based to
 scaffold-based.
 """
+from collections import defaultdict
+from typing import Iterator, List, TextIO, Union
 
-import agp
+from agp import AgpRow
+from agp.bed import BedRange
 
 
 class NoSuchContigError(Exception):
@@ -14,36 +17,136 @@ class NoSuchContigError(Exception):
         return 'Cannot find contig called "{}" in AGP'.format(self.contig_name)
 
 
-def run(bed_in, agp_in, bed_out):
-    # this dict maps contig name to a tuple containing:
-    # 0) scaffold name (str),
-    # 1) start position of contig on scaffold (int),
-    # 2) end position of contig on scaffold (int), and
-    # 3) orientation of contig on scaffold (str)
-    contig_to_scaffold = {}
-    for row in (r for r in agp_in if not (agp.is_string(r) or r.is_gap)):
-        contig_to_scaffold[row.component_id] = (
-            row.object,
-            row.object_beg,
-            row.object_end,
-            row.orientation,
-        )
+class UnsupportedOperationError(Exception):
+    """Error for trying to transform an unsupported style of bed row"""
 
+    pass
+
+
+class CoordinateNotFoundError(Exception):
+    """Error for bad coordinates specified"""
+
+    pass
+
+
+class BadOrientationError(Exception):
+    """Error for bad orientation (not +/-)"""
+
+    pass
+
+
+def create_contig_dict(agp_in: Iterator[Union[str, AgpRow]]) -> dict[str, List[AgpRow]]:
+    """
+    Load the agp file into a dictionary mapping contig name to a list
+    of rows in which that contig is the component_id.
+
+    Args:
+        agp_in: an agp file to read into a dictionary
+
+    Returns:
+        a dict mapping contig name to a list of rows in which that
+        contig is the component_id
+    """
+    contig_dict: dict[str, List[AgpRow]] = defaultdict(list)
+    for row in (r for r in agp_in if isinstance(r, AgpRow) and not r.is_gap):
+        contig_dict[row.component_id].append(row)
+    return contig_dict
+
+
+def transform_single_position(position: int, agp_row: AgpRow) -> int:
+    """Transform a position from component to object coordinates
+
+    Given a position in component coordinates (e.g., a position on a
+    contig), and the agp row containing that position, transform the
+    position to object coordinates (e.g., the same position, but on
+    a scaffold of which that contig is a part).
+
+    Args:
+        position: a position in component/contig coordinates
+        agp_row: the row containing that position
+
+    Returns:
+        the same position, but in object/scaffold coordinates
+    """
+
+    # first, offset the position by where it is on this _row_. This
+    # is necessary because not all rows contain an entire component/
+    # contig. E.g., if this row contains positions 101-200 on the
+    # contig, then position 150 on the contig is the 50th base in
+    # the piece of the contig enclosed by this row
+    position = position - agp_row.component_beg + 1
+    if agp_row.orientation == "+":
+        # then, offset the position by where the row is on the object/
+        # scaffold
+        position += agp_row.object_beg - 1
+    elif agp_row.orientation == "-":
+        position = agp_row.object_end - position + 1
+    else:
+        raise BadOrientationError(f"Orientation must be +/-: {agp_row}")
+    return position
+
+
+def find_agp_row(
+    contig_name: str, coordinate_on_contig: int, contig_dict: dict[str, List[AgpRow]]
+) -> AgpRow:
+    """Find the agp row containing a coordinate.
+
+    Given a dictionary mapping contig name to a list of agp rows
+    containing that contig, and a single position in contig coordinates,
+    find and return the AGP row containing that position.
+
+    Args:
+        contig_name:
+            the contig containing the coordinate
+        coordinate_on_contig:
+            the position to look for, in contig coordinates
+        contig_dict:
+            a dictionary mapping contig name to a list of agp rows
+            containing that contig
+
+    Returns:
+        a single AGP row containing the requested position
+    """
+    contig_list = contig_dict[contig_name]
+    if not contig_list:
+        raise NoSuchContigError(contig_name)
+    for agp_row in contig_list:
+        if (
+            agp_row.component_beg < coordinate_on_contig
+            and agp_row.component_end > coordinate_on_contig
+        ):
+            return agp_row
+
+    raise CoordinateNotFoundError(f"{contig_name}:{coordinate_on_contig}")
+
+
+def run(
+    bed_in: Iterator[BedRange], agp_in: Iterator[Union[str, AgpRow]], bed_out: TextIO
+):
+    contig_dict = create_contig_dict(agp_in)
     for bed_row in bed_in:
-        try:
-            scaffold, start, end, orient = contig_to_scaffold[bed_row.chrom]
-            bed_row.chrom = scaffold
-            if orient == "-":
-                tmp_end = end - bed_row.start + 1
-                tmp_start = end - bed_row.end + 1
-                bed_row.start, bed_row.end = tmp_start, tmp_end
-                if bed_row.strand == "-":
-                    bed_row.strand = "+"
-                elif bed_row.strand == "+":
-                    bed_row.strand = "-"
-            else:
-                bed_row.start += start - 1
-                bed_row.end += start - 1
-            print(bed_row, file=bed_out)
-        except KeyError as ke:
-            raise NoSuchContigError(bed_row.chrom) from ke
+        if not (bed_row.start and bed_row.end):
+            raise UnsupportedOperationError("Transforming coordinateless bed row")
+        agp_row_start = find_agp_row(bed_row.chrom, bed_row.start, contig_dict)
+        agp_row_end = find_agp_row(bed_row.chrom, bed_row.end, contig_dict)
+        if agp_row_start.object == agp_row_end.object:
+            new_start, new_end = sorted(
+                [
+                    transform_single_position(bed_row.start, agp_row_start),
+                    transform_single_position(bed_row.end, agp_row_end),
+                ]
+            )
+            new_orientation = None
+            if agp_row_start == agp_row_end and bed_row.strand:
+                if agp_row_start.orientation == "+":
+                    new_orientation = bed_row.strand
+                else:
+                    if bed_row.strand == "+":
+                        new_orientation = "-"
+                    else:
+                        new_orientation = "+"
+
+            print(
+                BedRange(agp_row_start.object, new_start, new_end, new_orientation),
+                file=bed_out,
+            )
--- tests/test_transform.py
+++ tests/test_transform.py
@@ -1,13 +1,35 @@
 from os.path import dirname, join
 
-from agp import open_agp
+import pytest
+
+from agp import AgpRow, open_agp
 from agp.bed import open_bed
-from agp.transform import run
+from agp.transform import run, transform_single_position
 
 
-def test_run_join(tmp_path):
-    bed_in_path = join(dirname(__file__), "data", "test.bed")
-    agp_in_path = join(dirname(__file__), "data", "test.agp")
+@pytest.mark.parametrize(
+    "in_position, agp_row, out_position",
+    [
+        (1, AgpRow("scaffold_16\t1\t167\t1\tW\tptg65l\t1\t167\t+"), 1),
+        (1, AgpRow("scaffold_16\t1\t167\t1\tW\tptg65l\t1\t167\t-"), 167),
+        (150, AgpRow("scaffold_16\t1\t167\t1\tW\tptg65l\t1\t167\t+"), 150),
+        (150, AgpRow("scaffold_16\t1\t167\t1\tW\tptg65l\t1\t167\t-"), 18),
+        (150, AgpRow("scaffold_16\t1\t167\t1\tW\tptg65l\t1\t167\t-"), 18),
+        (1, AgpRow("scaffold_16\t168\t200\t1\tW\tptg66l\t1\t33\t+"), 168),
+        (1, AgpRow("scaffold_16\t168\t200\t1\tW\tptg66l\t1\t33\t-"), 200),
+        (33, AgpRow("scaffold_16\t168\t200\t1\tW\tptg66l\t1\t33\t-"), 168),
+        (33, AgpRow("scaffold_16\t168\t200\t1\tW\tptg66l\t1\t33\t+"), 200),
+        (44, AgpRow("scaffold_16\t131\t150\t1\tW\tptg66l\t31\t50\t+"), 144),
+        (44, AgpRow("scaffold_16\t131\t150\t1\tW\tptg66l\t31\t50\t-"), 137),
+    ],
+)
+def test_transform_single_position(in_position, agp_row, out_position):
+    assert transform_single_position(in_position, agp_row) == out_position
+
+
+def test_run_transform(tmp_path):
+    bed_in_path = join(dirname(__file__), "data", "test_transform.bed")
+    agp_in_path = join(dirname(__file__), "data", "test_transform.agp")
     with open(tmp_path / "test_out.bed", "w") as outfile:
         run(open_bed(bed_in_path), open_agp(agp_in_path), outfile)
 
